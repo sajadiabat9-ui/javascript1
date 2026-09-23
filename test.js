e5b4ddf4a15f83f7cbb5d155d2ed59857191a641
sajadiabat <sajadiabat9@gmail.com>
2026-09-23 22:04:55 +0300

    my updates

--- test.js
+++ test.js
@@ -373,18 +373,63 @@ function returnColor() {
 
 
 
-// Q image
- let image = document.getElementById("myImage");
+
+
+
+//vedios
+// let textBox = document.getElementById("textBox");s
+// 1. changeFontFamily()
+function changeFontFamily() {
+    let fontFamilySelect = document.getElementById("fontFamily");
+    textBox.style.fontFamily = fontFamilySelect.value;
+}
+
+// 2. changeFontSize()
+function changeFontSize() {
+    let fontSizeSelect = document.getElementById("fontSize");
+    textBox.style.fontSize = fontSizeSelect.value;
+}
+
+// 3. (Italic)
+function toggleItalic() {
+    let italicCheck = document.getElementById("italicCheck");
+    if (italicCheck.checked) {
+        textBox.style.fontStyle = "italic";
+    } else {
+        textBox.style.fontStyle = "normal";
+    }
+}
+
+// 4.(Bold)
+function toggleBold() {
+    let boldCheck = document.getElementById("boldCheck");
+    if (boldCheck.checked) {
+        textBox.style.fontWeight = "bold";
+    } else {
+        textBox.style.fontWeight = "normal";
+    }
+}
+
+
+// 5. (Underline)
+function toggleUnderline() {
+    let underlineCheck = document.getElementById("underlineCheck");
+    if (underlineCheck.checked) {
+        textBox.style.textDecoration = "underline";
+    } else {
+        textBox.style.textDecoration = "none";
+    }
+}
+
+
+
+// image ex :
+let myImage = document.getElementById("myImage");
 
 function makeCircle() {
-    image.style.borderRadius = "50%";
+    myImage.style.borderRadius = "50%"; 
 }
 
 function makeSquare() {
-    image.style.borderRadius = "0";
- }
-
-
-
-
-
+    myImage.style.borderRadius = "0px"; 
+}
